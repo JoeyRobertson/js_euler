23f163261281d9560aa70f7f8761945731fa2ea2
Robertson <jrobertson@communityeducation.com>
2017-08-24 11:25:22 -0500

    completed the fibonacci problem

--- fibonacci.js
+++ fibonacci.js
@@ -1,16 +1,14 @@
-var total = 0;
-var lastNum = 0;
-var fib = 0;
-var currentNum = 1;
+var fib = [0,1];
+var i = 0;
+var sum = 0;
+while (fib[0] + fib[1] < 4000000){
+  i = fib[0] + fib[1];
+  fib[0] = fib[1];
+  fib[1] = i;
 
-while(currentNum <= 4000000){
-  currentNum = lastNum + currentNum;
-  fib = currentNum;
-  if(currentNum % 2 === 0){
-    total += currentNum;
+  if(i % 2 === 0){
+    sum+=i;
   }
-  lastNum = currentNum;
-  console.log(currentNum);
 }
 
-console.log(total);
+console.log(sum);
